479b1cffaa2d769a47e161774f56bd312d431b0b
Mitchell <Mitchell@128.4.204.72>
2017-02-15 10:59:02 -0500

    Please work

---
+++ MitchellLandLab1/src/cMain.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class cMain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter how many years do you plan on working?");
+		double years_worked = scan.nextDouble();
+		System.out.println("Enter your expected annual return in percent while you are working:" );
+		double annualreturn = scan.nextDouble();
+		System.out.println("Enter how many years you want to be retired:");
+		double yearsretired = scan.nextDouble();
+		System.out.println("Enter your expected annual return in percent when retired:");
+		double annualreturnretired = scan.nextDouble();
+		System.out.println("Enter your required Income:");
+		double requiredincome = scan.nextDouble();
+		System.out.println("Enter your monthly SSI:");
+		double MonthlySSI = scan.nextDouble();
+		
+		
+	
+	
+		
+		
+		
+	}
+
+}
